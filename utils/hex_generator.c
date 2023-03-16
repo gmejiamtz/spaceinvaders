887afee59aedb33bab0ca4165c684d7179d57efa
edro360 <redwin925a@gmail.com>
2023-03-15 18:24:03 -0700

    Added a hex generator.

---
+++ utils/hex_generator.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void hex_generator (char *filename, int depth_p, int width_p, int fresh) {
+    FILE *fp;
+    width_p = width_p / 8;
+    fp = fopen(filename, "w");
+    if (fp == NULL) {
+        printf("Could not open file %s\n", filename);
+        return;
+    }
+    srand(time(NULL));
+    for (int i = 0; i < depth_p; i++) {
+        for (int j = 0; j < width_p; j++) {
+            int num;
+            if (fresh) {
+                num = 0;
+            } else {
+                num = rand() % 256;
+            }
+            if (j == width_p-1) {
+                fprintf(fp, "%02X", num);
+            } else {
+                fprintf(fp, "%02X ", num);
+            }
+        }
+        if (i != depth_p-1) {
+            fprintf(fp, "\n");
+        }
+    }
+    fclose(fp);
+    return;
+}
+
+int main() {
+    hex_generator("memory_init_file.hex", 16, 8, 1);
+    return 0;
+}
