d73f0f4956a99cdd52c1d8303773d110f7cb3f8c
edro360 <redwin925a@gmail.com>
2023-03-19 16:22:14 -0700

    Organized the file and moved the bullet FSM to its own always_comb block.

--- player/player.sv
+++ player/player.sv
@@ -1,31 +1,30 @@
 module player 
 	#(parameter [11:0] color_p = {4'h5, 4'hE, 4'h5})
 	//parameter bus is color in {Red,Green,Blue} format
-	
-	(input [0:0] clk_i 			//clock
-	,input [0:0] reset_i		//reset button
-	,input [0:0] move_left_i 	//move left - left button
-	,input [0:0] shoot_i 		//shoot and start and resume levels - center button
-	,input [0:0] frame_i		//input for a frame
-	,input [0:0] move_right_i 	//move right -right button
-	,input [0:0] hit_i 			//hit by enemy
-	,input [0:0] hit_enemy_i	//player shoot enemy and can shoot again
-	,input [0:0] add_life_i		//add a life due to beating levels
-	,output [0:0] alive_o		//player has more than 0 lives
-	,output [9:0] pos_left_o	//left most position of player
-	,output [9:0] pos_right_o	//right most position of player
-	,output [9:0] gun_left_o	//location of gun, half of the ship size plus 1
-	,output [9:0] gun_right_o	//location of gun
-	,output [3:0] player_red_o	//ammount of red the player is for display
-	,output [3:0] player_green_o//ammount of green the player is for display
-	,output [3:0] player_blue_o	//ammount of blue the player is for display
-	,output [4:0] next_states_o	//outputs next states for debugging
-	,output [4:0] pres_states_o	//outputs present states for debugging
-	,output [0:0] bullet_o		//high when bullet flying
-	,output [9:0] bullet_left_o //left side of bullet
-	,output [9:0] bullet_right_o//right side of bullet
-	,output [9:0] bullet_top_o  //top of bullet
-	,output [9:0] bullet_bot_o	//bot of bullet
+	(input  [0:0] clk_i 		 //clock
+	,input  [0:0] reset_i		 //reset button
+	,input  [0:0] move_left_i 	 //move left - left button
+	,input  [0:0] shoot_i 		 //shoot and start and resume levels - center button
+	,input  [0:0] frame_i		 //input for a frame
+	,input  [0:0] move_right_i 	 //move right -right button
+	,input  [0:0] hit_i 		 //hit by enemy
+	,input  [0:0] hit_enemy_i	 //player shoot enemy and can shoot again
+	,input  [0:0] add_life_i	 //add a life due to beating levels
+	,output [0:0] alive_o		 //player has more than 0 lives
+	,output [9:0] pos_left_o	 //left most position of player
+	,output [9:0] pos_right_o	 //right most position of player
+	,output [9:0] gun_left_o	 //location of gun, half of the ship size plus 1
+	,output [9:0] gun_right_o	 //location of gun
+	,output [3:0] player_red_o	 //ammount of red the player is for display
+	,output [3:0] player_green_o //ammount of green the player is for display
+	,output [3:0] player_blue_o	 //ammount of blue the player is for display
+	,output [4:0] next_states_o	 //outputs next states for debugging
+	,output [4:0] pres_states_o	 //outputs present states for debugging
+	,output [0:0] bullet_o		 //high when bullet flying
+	,output [9:0] bullet_left_o  //left side of bullet
+	,output [9:0] bullet_right_o //right side of bullet
+	,output [9:0] bullet_top_o   //top of bullet
+	,output [9:0] bullet_bot_o	 //bot of bullet
 	,output [1:0] bullet_pres_o
 	,output [1:0] bullet_next_o
 	,output [0:0] bullet_not_border
@@ -47,56 +46,54 @@ module player
 	
 	//state enum for player state machine
 	typedef enum logic [4:0] {
-		player_state_failed	   =	5'b00000, //state 5
-		not_moving_and_alive   = 	5'b00001, //state 0
-		moving_left_and_alive  = 	5'b00010, //state 1
-		moving_right_and_alive = 	5'b00100, //state 2
-		player_shot_and_alive  =	5'b01000, //state 3
-		player_shot_and_dead   = 	5'b10000  //state 4
-	}states; 
+		ERROR	   =	5'b00000, //state 5
+		NOT_MOVING_AND_ALIVE   = 	5'b00001, //state 0
+		MOVING_LEFT_AND_ALIVE  = 	5'b00010, //state 1
+		MOVING_RIGHT_AND_ALIVE = 	5'b00100, //state 2
+		PLAYER_SHOT_AND_ALIVE  =	5'b01000, //state 3
+		PLAYER_SHOT_AND_DEAD   = 	5'b10000  //state 4
+	} states; 
+
 	typedef	enum logic [1:0] {
-		bullet_can_shoot = 2'b01,
-		bullet_is_flying = 2'b10
-	}bullet_state;
+		BULLET_CAN_SHOOT = 2'b01,
+		BULLET_IS_FLYING = 2'b10
+	} bullet_state;
 	
 	
 	//state busses
 	logic [4:0] present_l,next_l;
 	//1 bit outputs
-	logic [0:0] alive_l,lose_life,reset_player_pos,player_left,player_right,
-		new_game_l,bullet_active,latch_shoot;
+	logic [0:0] alive_l, lose_life, reset_player_pos,
+				player_left, player_right,
+				new_game_l, bullet_active, latch_shoot;
 	//position busses 
-	logic [9:0] left_l,right_l,gun_pos_l,gun_pos_r,
-		step_left,left_reset,bullet_pres_top,
-		bullet_pres_bot,
-		bullet_pres_left;
+	logic [9:0] left_l, right_l, gun_pos_l, gun_pos_r,
+				step_left, left_reset, bullet_pres_top,
+				bullet_pres_bot, bullet_pres_left;
 	//lives counter output
-	logic [1:0] lives_counter_l,live_step,live_reset,
-		bullet_next,bullet_pres;
+	logic [1:0] lives_counter_l, live_step, live_reset,
+				bullet_next, bullet_pres;
 	//left border max
 	localparam left_border = 9;
 	localparam right_border = 629;
 
 	//bullet info
-	logic [0:0] bullet_hit_something,bullet_move_up,reset_bullet;
+	logic [0:0] bullet_hit_something, bullet_move_up, reset_bullet;
 	
-
-
 	//state machine always_ff block
 	//resets to 5'b00001
 	always_ff @(posedge clk_i) begin
 		if (reset_i) begin
 			present_l <= 5'b00001;
 		end else begin
-
 			present_l <= next_l;
-			//assert (next_l != player_state_failed) else 
-			//$display("Asserted next_l != player_state_failed! State has been lost!");
-
+			// DEBUGGING PURPOSES:
+			//assert (next_l != ERROR) else 
+			//$display("Asserted next_l != ERROR! State has been lost!");
 		end
 	end
 
-	//bullet fsm and info needed
+	//bullet fsm and bullet is in the air
 	always_ff @(posedge clk_i) begin
 		if (~bullet_active) begin
 			bullet_pres_left <= left_l + 10'd17;
@@ -107,11 +104,13 @@ module player
 
 	always_ff @(posedge clk_i) begin
 		if(reset_i) begin
-			bullet_pres <= bullet_can_shoot;
+			bullet_pres <= BULLET_CAN_SHOOT;
 		end else begin
 			bullet_pres <= bullet_next;
 		end
 	end
+
+	// Flip Flop to hold the shoot button until it hits an enemy or hits the top border.
 	always_ff @(posedge clk_i) begin
 		if(~shoot_i & (bullet_pres_top == 10'd424) | (bullet_pres_top <= 10'd10)) begin
 			latch_shoot <= 1'b0;
@@ -169,7 +168,7 @@ module player
 		/* verilator lint_on PINCONNECTEMPTY */
 
 
-	//combinational logic for next states
+	// Combinatory Player FSM for next states
 	always_comb begin
 		right_l = left_l + 10'd40;
 		gun_pos_l = (left_l + 10'd15);
@@ -181,184 +180,181 @@ module player
 		reset_player_pos = 1'b1;
 		player_right = 1'b0;
 		case (present_l)
-			not_moving_and_alive: 
+			NOT_MOVING_AND_ALIVE: 
 			begin
 				//stays in state 0
 				reset_player_pos = 1'b0;
-				if(~hit_i &  (
-				~(move_right_i ^ move_left_i) | 
-				move_left_i & (left_border >= left_l) | move_right_i & (right_border <= right_l))) begin
-					next_l = not_moving_and_alive;
+				if(~hit_i &  (~(move_right_i ^ move_left_i) | 
+								move_left_i & (left_border >= left_l) | 
+								move_right_i & (right_border <= right_l))) begin
+					next_l = NOT_MOVING_AND_ALIVE;
 				end
 				//moves left
 				else if(~hit_i & ~move_right_i & move_left_i) begin
 					player_left = 1'b1;
-					next_l = moving_left_and_alive; 
+					next_l = MOVING_LEFT_AND_ALIVE; 
 				end
 				//move right
 				else if(~hit_i & move_right_i & ~move_left_i) begin
 					player_right = 1'b1;
-					next_l = moving_right_and_alive; 
+					next_l = MOVING_RIGHT_AND_ALIVE; 
 				end
 				//need logic to get hit by enemy
 				else if(hit_i & (lives_counter_l > 2'b00)) begin
 					lose_life = 1'b1;
-					next_l = player_shot_and_alive;
+					next_l = PLAYER_SHOT_AND_ALIVE;
 				end
 				//player loses game 
 				else if(hit_i & (lives_counter_l == 2'b00)) begin
 					alive_l = 1'b0;
-					next_l = player_shot_and_dead;
+					next_l = PLAYER_SHOT_AND_DEAD;
 				end 
 				//error happened and go to lost game state
 				else begin
-					next_l = player_state_failed;
+					next_l = ERROR;
 				end
 			end
-
-			moving_left_and_alive: begin
+			MOVING_LEFT_AND_ALIVE: begin
 				player_left = 1'b1;
 				reset_player_pos = 1'b0;
 				//stay in state 1 - move left
 				if(~hit_i & move_left_i & ~move_right_i & (left_border < left_l)) begin
-					next_l = moving_left_and_alive;
+					next_l = MOVING_LEFT_AND_ALIVE;
 				end
 				//go back to state 0 - dont move 
 				else if(~hit_i & (move_left_i & ~move_right_i & (left_border >= left_l) |
 				~(move_left_i ^ move_right_i))) begin
 					player_left = 1'b0;
-					next_l = not_moving_and_alive;
+					next_l = NOT_MOVING_AND_ALIVE;
 				end
 				//go to state 2 - move right
 				else if(~move_left_i & move_right_i & ~hit_i) begin
 					player_left = 1'b0;
 					player_right = 1'b1;
-					next_l = moving_right_and_alive;
+					next_l = MOVING_RIGHT_AND_ALIVE;
 				end
 				//go to state 3 - player shot but still has lives
 				else if(hit_i & (lives_counter_l > 2'b00)) begin
 					player_left = 1'b0;
 					lose_life = 1'b1;
-					next_l = player_shot_and_alive;
+					next_l = PLAYER_SHOT_AND_ALIVE;
 				end
 				//go to state 4 - player shot and has no lives
 				else if(hit_i & (lives_counter_l == 2'b00)) begin
 					player_left = 1'b0;
 					alive_l = 1'b0;
-					next_l = player_shot_and_dead;
+					next_l = PLAYER_SHOT_AND_DEAD;
 				end else begin
-					next_l = player_state_failed;
+					next_l = ERROR;
 				end
 			end
 
-			moving_right_and_alive: begin
+			MOVING_RIGHT_AND_ALIVE: begin
 				player_right = 1'b1;
 				//stay in state 1 - move left
 				reset_player_pos = 1'b0;
 				if(~hit_i & move_left_i & ~move_right_i) begin
 					player_right = 1'b0;
 					player_left = 1'b1;
-					next_l = moving_left_and_alive;
+					next_l = MOVING_LEFT_AND_ALIVE;
 				end
 				//go back to state 0 - dont move 
 				else if(~hit_i & (move_left_i & ~move_right_i &
 					(right_border <= right_l) |
 					~(move_left_i ^ move_right_i))) begin
 					player_right = 1'b0;
-					next_l = not_moving_and_alive;
+					next_l = NOT_MOVING_AND_ALIVE;
 				end
 				//go to state 2 - move right
 				else if(~hit_i & (~move_left_i & move_right_i & 
 					(right_border > right_l))) begin
-					next_l = moving_right_and_alive;
+					next_l = MOVING_RIGHT_AND_ALIVE;
 				end
 				//go to state 3 - player shot but still has lives
 					else if(hit_i & (lives_counter_l > 2'b00)) begin
 					lose_life = 1'b1;
 					player_right = 1'b0;
-					next_l = player_shot_and_alive;
+					next_l = PLAYER_SHOT_AND_ALIVE;
 				end
 				//go to state 4 - player shot and has no lives
 				else if(hit_i & (lives_counter_l == 2'b00)) begin
 					alive_l = 1'b0;
 					player_right = 1'b0;
-					next_l = player_shot_and_dead;
+					next_l = PLAYER_SHOT_AND_DEAD;
 				end else begin
-					next_l = player_state_failed;
+					next_l = ERROR;
 				end
 			end
 		
-			player_shot_and_alive: begin
+			PLAYER_SHOT_AND_ALIVE: begin
 			reset_player_pos = 1'b0;
 			if(shoot_i & ~(move_left_i ^ move_right_i)) begin
 				reset_player_pos = 1'b1;
-				next_l = not_moving_and_alive;
+				next_l = NOT_MOVING_AND_ALIVE;
 			end else if (shoot_i & move_left_i & ~move_right_i) begin
 				reset_player_pos = 1'b1;
 				player_left = 1'b1;
-				next_l = moving_left_and_alive;
+				next_l = MOVING_LEFT_AND_ALIVE;
 			end else if (shoot_i & ~move_left_i & move_right_i) begin
 				reset_player_pos = 1'b1;
 				player_right = 1'b1;
-				next_l = moving_right_and_alive;
+				next_l = MOVING_RIGHT_AND_ALIVE;
 			end else begin //i dont think this state can transition to the error state
-				next_l = player_shot_and_alive;
+				next_l = PLAYER_SHOT_AND_ALIVE;
 			end
 			end
 
-			player_shot_and_dead: begin
-			alive_l = 1'b0;
-			reset_player_pos = 1'b0;
-			if(shoot_i & ~(move_left_i ^ move_right_i)) begin
-				reset_player_pos = 1'b1;
-				alive_l = 1'b1;
-				new_game_l = 1'b1;
-				next_l = not_moving_and_alive;
-			end else if (shoot_i & move_left_i & ~move_right_i) begin
-				reset_player_pos = 1'b1;
-				player_left = 1'b1;
-				alive_l = 1'b1;
-				new_game_l = 1'b1;
-				next_l = moving_left_and_alive;
-			end else if (shoot_i & ~move_left_i & move_right_i) begin
-				reset_player_pos = 1'b1;
-				player_right = 1'b1;
-				alive_l = 1'b1;
-				new_game_l = 1'b1;
-				next_l = moving_right_and_alive;
-			end else begin //i dont think this state can transition to the error state
-				next_l = player_shot_and_dead;
+			PLAYER_SHOT_AND_DEAD: begin
+				alive_l = 1'b0;
+				reset_player_pos = 1'b0;
+				if(shoot_i & ~(move_left_i ^ move_right_i)) begin
+					reset_player_pos = 1'b1;
+					alive_l = 1'b1;
+					new_game_l = 1'b1;
+					next_l = NOT_MOVING_AND_ALIVE;
+				end else if (shoot_i & move_left_i & ~move_right_i) begin
+					reset_player_pos = 1'b1;
+					player_left = 1'b1;
+					alive_l = 1'b1;
+					new_game_l = 1'b1;
+					next_l = MOVING_LEFT_AND_ALIVE;
+				end else if (shoot_i & ~move_left_i & move_right_i) begin
+					reset_player_pos = 1'b1;
+					player_right = 1'b1;
+					alive_l = 1'b1;
+					new_game_l = 1'b1;
+					next_l = MOVING_RIGHT_AND_ALIVE;
+				end else begin //i dont think this state can transition to the error state
+					next_l = PLAYER_SHOT_AND_DEAD;
+				end
 			end
-			
-			end
-		//error state if in no state
-			player_state_failed:
-			next_l = player_state_failed;
-		//if in multple states at the same time
-			default:
-			next_l = present_l;
-			
+			ERROR: //error state if in no state
+				next_l = ERROR;
+			default: // if in multple states at the same time
+				next_l = present_l;
 		endcase
+	end
 
+	// Combinatory Player's Bullet FSM for next states
+	always_comb begin
 		bullet_move_up = 1'b0;
 		bullet_hit_something = 1'b0;
 		reset_bullet = 1'b0;
 		bullet_active = 1'b0;
 		case (bullet_pres)
-			bullet_can_shoot: begin
+			BULLET_CAN_SHOOT: begin
 				if(latch_shoot) begin
 					bullet_active = 1'b1;
 					bullet_move_up = 1'b1;
-					bullet_next = bullet_is_flying;
+					bullet_next = BULLET_IS_FLYING;
 				end else begin
 					reset_bullet = 1'b1;
 					bullet_move_up = 1'b0;
 					bullet_active = 1'b0;
-					bullet_next = bullet_can_shoot;
+					bullet_next = BULLET_CAN_SHOOT;
 				end
 			end
-
-			bullet_is_flying: begin
+			BULLET_IS_FLYING: begin
 				bullet_move_up = 1'b1;
 				reset_bullet = 1'b0;
 				bullet_active = 1'b1;
@@ -366,18 +362,16 @@ module player
 					bullet_move_up = 1'b1;
 					reset_bullet = 1'b0;
 					bullet_active = 1'b1;
-					bullet_next = bullet_is_flying;
+					bullet_next = BULLET_IS_FLYING;
 				end else begin
 					bullet_active = 1'b0;
 					reset_bullet = 1'b1;
 					bullet_move_up = 1'b0;
-					bullet_next = bullet_can_shoot;
+					bullet_next = BULLET_CAN_SHOOT;
 				end
 			end
-
 			default:
 				bullet_next = bullet_pres;
-			
 		endcase
 	end
 
